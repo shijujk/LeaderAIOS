9dd01d5bdc2c4b1568a56ecdc329864024713f2e
shijujk <97731119+shijujk@users.noreply.github.com>
2026-01-01 14:57:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/LeaderAIOS/",
-  server: {
-    host: true,
-    allowedHosts: true
-  }
+  base: "/LeaderAIOS/"
 });
